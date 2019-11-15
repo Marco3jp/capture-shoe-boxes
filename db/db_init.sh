5f36e58be5dab4a12f48c268676dd814e6b8a614
marco <28888207+Marco3jp@users.noreply.github.com>
2019-11-15 14:56:56 +0900

    設定ファイルの切り出しをした

--- db/db_init.sh
+++ db/db_init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export ROOT_PASSWORD=examplePassword # please rewrite your password
 mysql -uroot -p$ROOT_PASSWORD -e "DROP DATABASE IF EXISTS is_exist_researcher; CREATE DATABASE is_exist_researcher;"
 mysql -uroot -p$ROOT_PASSWORD -e "DROP USER IF EXISTS capture_shoe_boxes@localhost; DROP USER IF EXISTS diff_shoe_boxes@localhost; DROP USER IF EXISTS is_exist_researcher_api@localhost;"
-mysql -uroot -p$ROOT_PASSWORD is_exist_researcher < ./tables.sql
-mysql -uroot -p$ROOT_PASSWORD < ./users.sql
+mysql -uroot -p$ROOT_PASSWORD is_exist_researcher <./tables.sql
+mysql -uroot -p$ROOT_PASSWORD <./users.sql
---
+++ db/secret.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# **PLEASE REWRITE YOUR PASSWORD**
+export CAPTURE_SHOE_BOX_SQL_PASSWORD=example_password
+export DIFF_SHOE_BOX_SQL_PASSWORD=example_password
+export IS_EXIST_RESEARCHER_API_SQL_PASSWORD=example_password
+export ROOT_PASSWORD=example_password
